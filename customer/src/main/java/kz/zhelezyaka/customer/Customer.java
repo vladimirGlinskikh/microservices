82b2e4cc2f7d02a26519e4cd7ad69eda3226dc1f
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-12-04 06:56:16 +0600

    Spring Data JPA in action

--- customer/src/main/java/kz/zhelezyaka/customer/Customer.java
+++ customer/src/main/java/kz/zhelezyaka/customer/Customer.java
@@ -1,12 +1,26 @@
 package kz.zhelezyaka.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence")
     private Integer id;
+
     private String firstname;
     private String lastname;
     private String email;
--- customer/src/main/java/kz/zhelezyaka/customer/CustomerService.java
+++ customer/src/main/java/kz/zhelezyaka/customer/CustomerService.java
@@ -3,12 +3,15 @@ package kz.zhelezyaka.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstname(request.firstname())
                 .lastname(request.lastname())
                 .email(request.email())
                 .build();
+        // todo: check if email valid
+        // todo: check if email not taken
+        customerRepository.save(customer);
     }
 }
